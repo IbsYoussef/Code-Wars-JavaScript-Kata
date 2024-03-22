edf8f1cba27b4e473cc7d4f2dd1ee1d6e302710c
IbsYoussef <ibraheemyoussefiy@gmail.com>
2024-03-22 21:39:42 +0000

    Reversed Strings 8kyu Kata

---
+++ 8 Kyu/reverseWords.js
@@ -0,0 +1,9 @@
+// 8 kyu Reversed Strings
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+const solution = str => str.split('').reverse().join('');
+
+console.log(solution('world')); // 'dlrow'
--- reverseWords.js
+++
@@ -1,5 +0,0 @@
-const reverseWords = (str) => {
-    return str.split('').reduce((r, c) => c + r, '')
-}
-
-console.log(reverseWords('The quick brown fox jumps over the lazy dog.')) 
