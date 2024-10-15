9ed7a594e6d9ef79751adf47a8e729eb6411147b
IbsYoussef <ibraheemyoussefiy@gmail.com>
2024-10-15 22:59:40 +0100

    S2 for FindDivisor kata (in progress)

---
+++ 7 Kyu/Find_the_divisors/FindTheDivisors2.js
@@ -0,0 +1,25 @@
+
+const divisors = (integer) => {
+    let isPrime = false;
+
+}
+
+// PseudoCode
+/* 
+1: Check if number is prime, if it is prime return prime message.
+
+First create a flag for the prime and set it to false, then check if number is prime, if true set to true and then return message.
+
+2: Once verified n is not prime, create array to store divisor results 
+
+3: Cycle through numbers divisors with a for loop
+
+4: Append each valid divisor to array 
+
+5: Return Array result
+
+
+
+
+
+*/
