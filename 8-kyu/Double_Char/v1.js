dd80d54964e8db01ddce8475eb2d1b5a49bc48b7
IbsYoussef <ibraheemyoussefiy@gmail.com>
2024-11-03 19:26:09 +0000

    Added link description to Double Char kata!

--- 8-kyu/Double_Char/v1.js
+++ 8-kyu/Double_Char/v1.js
@@ -1,6 +1,6 @@
 /*
 Kata: Double Char
-Link: 
+Link: https://www.codewars.com/kata/56b1f01c247c01db92000076/javascript
 Description: Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
 Examples (Input -> Output):
