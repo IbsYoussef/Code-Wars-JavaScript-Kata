faca1534b1aa849e35eb3f16d1f1aea543a9b1a1
IbsYoussef <ibraheemyoussefiy@gmail.com>
2024-11-03 19:39:23 +0000

    Added Link to Kata

--- 8-kyu/Opposite_Number/v1.js
+++ 8-kyu/Opposite_Number/v1.js
@@ -1,6 +1,6 @@
 /*
 Kata: Opposite Number
-Link:
+Link: https://www.codewars.com/kata/56dec885c54a926dcd001095/javascript
 Description: Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
 
 Examples:
