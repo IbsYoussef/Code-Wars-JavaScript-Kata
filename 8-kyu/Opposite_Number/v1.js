e72da53b27e1d4e55af616d32c423de81453e1e4
IbsYoussef <ibraheemyoussefiy@gmail.com>
2024-11-03 19:38:22 +0000

    Opposite Number Kata v1

---
+++ 8-kyu/Opposite_Number/v1.js
@@ -0,0 +1,18 @@
+/*
+Kata: Opposite Number
+Link:
+Description: Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+const opposite = (number) => -number;
+
+// Test cases
+console.log(opposite(1)); // -1
+console.log(opposite(14)); // -14
+console.log(opposite(-34)); // 34
